42cd55595e762a7cfafe59afb47fa240e4d9c2a3
MyName <you@example.com>
2024-11-18 16:29:28 +0400

    work verson2

--- src/javaapplication2/NewJPanel.java
+++ src/javaapplication2/NewJPanel.java
@@ -2,6 +2,7 @@ package javaapplication2;
 import java.awt.Panel;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 
 public class NewJPanel extends Panel {
     private int myX;
@@ -9,12 +10,13 @@ public class NewJPanel extends Panel {
     private int mySize;
     
     public void paint(Graphics g){
-    g.setColor(Color.black);
-    g.fillOval(60, 105, 120, 100);
-    g.setColor(Color.black);
-    g.fillOval(45, 105, 30, 100);
+    
     g.setColor(Color.red);
-    g.fillOval(130, 105, 30, 100);
+    g.fillRect(80, 85, 120, 60);
+    g.setColor(Color.black);
+    g.fillRect(200, 145, 120, 60);
+    g.setColor(Color.blue);
+    g.fillOval(140, 85, 120, 120);
     }
     
     
@@ -25,6 +27,12 @@ public class NewJPanel extends Panel {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                formKeyPressed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -37,6 +45,35 @@ public class NewJPanel extends Panel {
             .addGap(0, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
+        switch (evt.getKeyCode()){
+            case KeyEvent.VK_RIGHT:
+                if (njp.getMyX()>0){
+                    njp.setMyX(njp.getMyX()-50);
+                }
+                break;
+            case KeyEvent.VK_LEFT:
+                if (njp.getMyX()>0){
+                    njp.setMyX(njp.getMyX()-50);
+                }
+                break;
+        case KeyEvent.VK_UP:
+                if (njp.getMyX()>0){
+                    njp.setMyX(njp.getMyX()-50);
+                }
+                break;
+               
+        case KeyEvent.VK_DOWN:
+                if (njp.getMyX()>0){
+                    njp.setMyX(njp.getMyX()-50);
+                }
+                break;
+        default:
+            throw new AssertionError();
+        }
+        repaint();
+    }//GEN-LAST:event_formKeyPressed
 
     /**
      * @return the myX
@@ -79,6 +116,8 @@ public class NewJPanel extends Panel {
     public void setMySize(int mySize) {
         this.mySize = mySize;
     }
+
+   
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
